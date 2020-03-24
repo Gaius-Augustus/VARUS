1f5f2ad5ad4851fffcd18fd2138d87a52423459e
Mario Stanke <mario.stanke@gmail.com>
2020-03-24 16:39:19 +0100

    start of fixing issue3

--- Implementation/src/Downloader.cpp
+++ Implementation/src/Downloader.cpp
@@ -8,6 +8,12 @@
 #include "../headers/Downloader.h"
 #include "../headers/debug.h"
 
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
 Downloader::Downloader(ParameterHandler *p) {
     param = p;
 }
@@ -22,11 +28,35 @@ std::string Downloader::shellCommand(Run *r){
     x << r->X;
     
     std::string s = param->fastqDumpCall + " -N " + n.str() + " -X " + x.str() + " -O " + param->outFileNamePrefix + r->accesionId + "/"
-	+ "N" + n.str() + "X" + x.str() + "/ --fasta " + r->accesionId;
+	+ "N" + n.str() + "X" + x.str() + "/ --fasta ";
     if (r->paired)
-	s += " --split-files";
+	s += " --split-files ";
     
+    s += r->accesionId;
     return s;
+}
+
+char const ** Downloader::shellCommand2(Run *r){
+    std::ostringstream n, x;
+    
+    n << r->N;
+    x << r->X;
+
+    size_t numargs = 5;
+    if (r->paired)
+        numargs += 1;
+
+    char const **args = new char const* [numargs];
+    
+    //args[0] = param->fastqDumpCall;
+    
+    // " -N " + n.str() + " -X " + x.str() + " -O " +
+    // param->outFileNamePrefix + r->accesionId + "/" + "N" + n.str() + "X" + x.str() + "/ --fasta ";
+//    if (r->paired)
+//	s += " --split-files ";
+    
+    //  s += r->accesionId;
+    return args;
 }
 
 void Downloader::nextBatchIndices(Run *r) {
@@ -62,10 +92,41 @@ bool Downloader::getBatch(Run *r, bool all){
     
     std::string s = shellCommand(r);
     
-    DEBUG(0,"getBatch(" << r->accesionId << ", X=" << r->X << ", N=" << r->N << "): " << s);
-
+    std::ostringstream msg;
+    msg << "getBatch(" << r->accesionId << ", X=" << r->X << ", N=" << r->N << "): " << s;
+    DEBUG(0,msg.str());
     int status = -1;
 
+    char * ls_args[11] = {"fastq-dump", "-N", "3750000", "-X", "3799999", "-O",
+                         "/home/mario/VARUS/example/Schizosaccharomyces_pombe/",
+                         "--fasta", "--split-files", "SRR9057308", NULL};
+    
+    pid_t c_pid, pid;
+
+    c_pid = fork();
+
+    if (c_pid == 0){
+        // CHILD
+        std::cout << "Child: executing" << std::endl;
+        execvp( ls_args[0], ls_args);
+        perror("execve failed");
+    }else if (c_pid > 0){
+        // PARENT 
+
+        if( (pid = wait(&status)) < 0){
+            perror("wait");
+            exit(1);
+        }
+        printf("Parent: finished\n");
+    }else{
+        perror("fork failed");
+        exit(1);
+    }
+
+/*
+    //system(("prefetch " + r->accesionId).c_str());
+    
+    
     while (status != 0 && ++attempt <= numberOfAttempts){
 	status = system(s.c_str()); // run fastq-dump
 	if (status != 0) {
@@ -77,6 +138,7 @@ bool Downloader::getBatch(Run *r, bool all){
 	DEBUG(0,"Definitively failed to save batch "  << r->accesionId << " -N  " << r->N << " -X " << r->X);
 	return false;
     }
+*/
     r->timesDownloaded++;
     
     return true;
